7eac95fee66eec53c9788db85f16a3a3c17c3922
Andrea Luciano <adlucio87@gmail.com>
2020-02-05 12:22:26 +0100

    add method for verifing credentials basic auth

--- app/controllers/CalibreCtrl.js
+++ app/controllers/CalibreCtrl.js
@@ -35,4 +35,42 @@ module.exports.ebookConvert = function (req, res) {
             });
     });
 };
+
+
+module.exports.ebookConvertBasicAuth = function (req, res) {
+	
+    // parse login and password from headers
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const strauth = new Buffer(b64auth, 'base64').toString()
+    const splitIndex = strauth.indexOf(':')
+    const login = strauth.substring(0, splitIndex)
+    const password = strauth.substring(splitIndex + 1)
+
+    //function to validate credentials using https://www.npmjs.com/package/tsscmp
+    //Prevents timing attacks using Brad Hill's Double HMAC pattern to perform secure string comparison
+    function check (name, pass) {
+      //var valid = true
+
+      return (name == 'sendtokindleapp_auth' && pass == '123sdkljfhsdkjfhsdkjhfsfkdjhf!!')
+      
+      // Simple method to prevent short-circut and use timing-safe compare
+      //valid = compare(name, 'sendtokindleapp_auth') && valid
+      //valid = compare(pass, '123sdkljfhsdkjfhsdkjhfsfkdjhf!!') && valid
+
+      //return valid
+    }
+
+    if (!check(login, password)) {
+      res.statusCode = 401
+      res.setHeader('WWW-Authenticate', 'Basic realm="example"')
+      res.end('Access denied')
+    }
+    ///////////////////////////////////////////////////////////////////////////
+	
+	ebookConvert(req,res);
+};
+
+
+
+
 	
